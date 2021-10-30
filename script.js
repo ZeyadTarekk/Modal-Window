b713f32202c4072c7b0a5f639f9b9935eb11abb1
Zeyad <zeyad.ta01@gmail.com>
2021-10-30 20:40:31 +0200

    Defining the keyHandle function outside the event listener

--- script.js
+++ script.js
@@ -21,9 +21,11 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
 
-document.addEventListener("keydown", function (e) {
+const keyHandle = function (e) {
   // console.log(e.key);
   if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
-});
+};
+
+document.addEventListener("keydown", keyHandle);
